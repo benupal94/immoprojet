2fb461733b63e2a004d794eb01fb748460d34d0f
bienvenu Palanga <bienvenupalanga1@gmail.com>
2020-02-17 19:17:01 +0100

    initialisation

---
+++ database/migrations/2020_02_14_163517_create_immo_groups_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImmoGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('immo_groups', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->decimal('prix');
+            $table->string('images');
+            $table->string('lotissement');
+            $table->string('description');
+            $table->string('avance');
+            $table->BigInteger('id_type')->unsigned()->index();
+            $table->BigInteger('id_proprietaire')->unsigned()->index();
+            $table->BigInteger('id_pays')->unsigned()->index();
+            $table->BigInteger('id_ville')->unsigned()->index();
+            $table->BigInteger('id_quartier')->unsigned()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('immo_groups');
+    }
+}
---
+++ database/migrations/2020_02_14_163621_create_proprietaires_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProprietairesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proprietaires', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('adresse');
+            $table->string('contact');
+            $table->string('personne_ressource');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proprietaires');
+    }
+}
